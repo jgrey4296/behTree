b2c5b6acdaca754ddbd4455c435a926348dc932f
John Grey <jgrey@ucsc.edu>
2016-04-11 13:47:22 -0700

    add more details to example behaviour

--- exampleBehaviours.js
+++ exampleBehaviours.js
@@ -9,23 +9,35 @@ define(['underscore'],function(_){
     //arrays of construction functions of behaviours:
     var BehaviourModule = [];
 
+    //initial tree
     BehaviourModule.push(function(bTree){
         console.log("Loading initial tree");
         bTree.Behaviour('initialTree')
             .children('initialBehaviour','secondBehaviour');
     });
-    
+
+    //initial behaviour
     BehaviourModule.push(function(bTree){
         console.log("Loading initial Behaviour");
         bTree.Behaviour('initialBehaviour')
             .entryAction(function(ctx){
                 if(ctx.values.name === 'bob'){
                     ctx.fb.assert(`.initialBehaviour.${ctx.values.name}`);
-                    console.log(`.initialBehaviour.${ctx.values.name}`);
+                    console.log(`.initialBehaviour.${ctx.values.name} entry`);
+                }else{
+                    ctx.fb.assert(`.initialBehaviour`);
+                    console.log(`.initialBehaviour entry`);
                 }
+            })
+            .performAction(function(ctx){
+                console.log("initial behaviour performing");
+            })
+            .exitAction(function(ctx){
+                console.log("initial behaviour exiting");
             });
     });
 
+    //second behaviour v1
     BehaviourModule.push(function(bTree){
         console.log("Loading second behaviour");
         bTree.Behaviour('secondBehaviour')
@@ -34,9 +46,13 @@ define(['underscore'],function(_){
             .entryAction(function(ctx){
                 ctx.fb.assert(`.secondBehaviour.${ctx.values.name}`);
                 console.log(`.secondBehaviour.${ctx.values.name}`);
-            });
+            })
+            .children("testChildBehaviour","testChildBehaviour")
+            .exitAction(d=>console.log("secondBehaviour exit"));
+        
     });
 
+    //second behaviour v2
     BehaviourModule.push(function(bTree){
         console.log("Loading alt second behaviour");
         bTree.Behaviour('secondBehaviour')
@@ -44,7 +60,14 @@ define(['underscore'],function(_){
             .entryAction(function(ctx){
                 ctx.fb.assert(`.secondBehaviour.alt.${ctx.values.name}`);
                 console.log(`.secondBehaviour.alt.${ctx.values.name}`);
-            });
+            })
+            .exitAction(d=>console.log("alt second behaviour exit"));
+    });
+
+    //testChildBehaviour
+    BehaviourModule.push(function(bTree){
+        bTree.Behaviour('testChildBehaviour')
+            .performAction(ctx=>console.log("test child behaviour"));
     });
     
     return BehaviourModule;
