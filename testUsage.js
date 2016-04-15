31f0f7a49b503d7a6b016955b10ac02f1f9970b4
John Grey <jgrey@ucsc.edu>
2016-04-15 01:11:34 -0700

    added initial facts, and a printout of all facts each turn

--- testUsage.js
+++ testUsage.js
@@ -15,11 +15,18 @@ let characters = [],
     turns = 10,
     currentTurn = 0;
 
+//assert initial facts:
+baseBTree.assert(".locations.kitchen",".locations.study",".locations.diningRoom",
+                 ".locations.kitchen.items.knife",".locations.diningRoom.items.spoon",
+                 ".bob.location!kitchen",".bill.location!kitchen");
+
 //Load the characters:
 characters.push(bob,bill);
 
 let intervalObject = setInterval(function(){
     console.log(`\nTurn ${currentTurn}`);
+    console.log('\tFacts:' + baseBTree.fb.toStrings().map(e=>`\n\t${e}`).join(""));
+    
     if(turns < currentTurn++){
         clearInterval(intervalObject);
     }
