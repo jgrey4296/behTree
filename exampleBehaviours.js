2ee954e5e0557783d07b3a94ca7cdf745bb9b8fe
John Grey <jgrey@ucsc.edu>
2016-04-15 01:11:07 -0700

    simple movement behaviour, tracked by exclusion logic

--- exampleBehaviours.js
+++ exampleBehaviours.js
@@ -7,68 +7,38 @@ if(typeof define !== 'function'){
 define(['underscore'],function(_){
 
     //arrays of construction functions of behaviours:
-    var BehaviourModule = [];
+    var BMod = [];
 
     //initial tree
-    BehaviourModule.push(function(bTree){
-        console.log("Loading initial tree");
+    BMod.push(function(bTree){
         bTree.Behaviour('initialTree')
-            .children('initialBehaviour','secondBehaviour');
+            .type('parallel')
+            .persistent(true)
+            .children('move');
     });
 
-    //initial behaviour
-    BehaviourModule.push(function(bTree){
-        console.log("Loading initial Behaviour");
-        bTree.Behaviour('initialBehaviour')
-            .entryAction(function(ctx){
-                if(ctx.values.name === 'bob'){
-                    ctx.fb.assert(`.initialBehaviour.${ctx.values.name}`);
-                    console.log(`.initialBehaviour.${ctx.values.name} entry`);
-                }else{
-                    ctx.fb.assert(`.initialBehaviour`);
-                    console.log(`.initialBehaviour non-specific entry`);
-                }
-            })
-            .performAction(function(ctx){
-                console.log("initial behaviour performing");
-            })
-            .exitAction(function(ctx){
-                console.log("initial behaviour exiting");
+    BMod.push(function(bTree){
+        bTree.Behaviour('move')//to kitchen
+            .specificity(2)
+            .persistent(true)
+            .entryCondition([`.locations.kitchen`,d=>`!!.${d.values.name}.location!kitchen`])
+            .entryAction(ctx=>console.log("(enter) move.s2"))
+            .performAction(ctx=>ctx.assert(`.${ctx.values.name}.location!kitchen`));
+    });
+
+    BMod.push(function(bTree){
+        bTree.Behaviour('move')//to diningRoom
+            .specificity(5)
+            .persistent(true)
+            .entryCondition([`.locations.diningRoom`,d=>`!!.${d.values.name}.location!diningRoom`])
+            .entryAction(ctx=>console.log("(enter) move.s5"))
+            .performAction(ctx=>{
+                ctx.assert(`.${ctx.values.name}.location!diningRoom`);
+                console.log(`${ctx.values.name} Moving to dining room`);
             });
     });
-
-    //second behaviour v1
-    BehaviourModule.push(function(bTree){
-        console.log("Loading second behaviour");
-        bTree.Behaviour('secondBehaviour')
-            .specificity(5)
-            .entryCondition(d=>`.initialBehaviour.${d.values.name}`)
-            .performAction(function(ctx){
-                ctx.fb.assert(`.secondBehaviour.${ctx.values.name}`);
-                console.log(`.secondBehaviour.${ctx.values.name} (perform)`);
-            })
-            .children("testChildBehaviour","testChildBehaviour")
-            .exitAction(d=>console.log("secondBehaviour exit"));
-        
-    });
-
-    //second behaviour v2
-    BehaviourModule.push(function(bTree){
-        console.log("Loading alt second behaviour");
-        bTree.Behaviour('secondBehaviour')
-            .specificity(0)
-            .performAction(function(ctx){
-                ctx.fb.assert(`.secondBehaviour.alt.${ctx.values.name}`);
-                console.log(`.secondBehaviour.alt.${ctx.values.name} (perform)`);
-            })
-            .exitAction(d=>console.log("alt second behaviour exit"));
-    });
-
-    //testChildBehaviour
-    BehaviourModule.push(function(bTree){
-        bTree.Behaviour('testChildBehaviour')
-            .performAction(ctx=>console.log("test child behaviour (perform)"));
-    });
+                         
     
-    return BehaviourModule;
+    
+    return BMod;
 });
